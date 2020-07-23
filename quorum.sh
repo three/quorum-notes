c1618bf786747fad42ffaecbd679eaf78cff465a
Eric Roberts <eric@quorum.us>
2020-07-23 08:44:34 -0400

    Update aliases

--- quorum.sh
+++ quorum.sh
@@ -39,5 +39,17 @@ alias jest='nvm run node "$QUORUM_ROOT/node_modules/.bin/jest"'
 alias jestd='nvm run node --inspect-brk "QUORUM_ROOT/node_modules/.bin/jest" --runInBand'
 # Docker compose with fullstack configuration
 alias fc='docker-compose -f docker-compose.yml -f docker/local/docker-compose.fullstack.yml'
+
+# Wake baement.0 from its slumber
+alias wakebasement='wakeonlan FC:AA:14:2A:AF:79'
 # Sync Development files to basement.0
-alias fullsync='rsync -rv --exclude-from "$QUORUM_TOOLS_DIR/rsync-exclude.txt" "$QUORUM_ROOT" "$QUORUM_REMOTE_SSH:$QUORUM_REMOTE_ROOT"'
+alias fullsync='rsync -rv --exclude-from="$QUORUM_TOOLS_DIR/rsync-exclude.txt" --exclude-from="$QUORUM_ROOT/.gitignore" "$QUORUM_ROOT" "$QUORUM_REMOTE_SSH:$QUORUM_REMOTE_ROOT"'
+
+# SSH into bastion with port forwarding to both dev and prod
+alias dbconnect='ssh -L 5433:quorum-where-the-magic-happens.ck4wgl7u5wcg.us-east-1.rds.amazonaws.com:5432 -L 5434:quorum-production.ck4wgl7u5wcg.us-east-1.rds.amazonaws.com:5432 bastion'
+
+# Docker compsoe with custom compose file and related commands
+alias dc='docker-compose -f "$HOME/docker-compose.yml"'
+alias up='docker-compose -f "$HOME/docker-compose.yml" up -d'
+alias logs='docker-compose -f "$HOME/docker-compose.yml" logs -f'
+alias reload='docker-compose -f "$HOME/docker-compose.yml" restart server frontend'
