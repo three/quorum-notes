96a0501a74eefb213703af3fbf2a91253e189e36
Eric Roberts <eric@quorum.us>
2021-03-18 14:23:27 -0400

    Update qring entrypoint

--- qring/entrypoint.sh
+++ qring/entrypoint.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env zsh
+DOCKER_HOST_IP="$(dig +short host.docker.internal)"
+[[ -n "$DOCKER_HOST_IP" ]] && echo -e "$(dig +short host.docker.internal)\tpostgres" >>/etc/hosts
+
 exec "$@"
