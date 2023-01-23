72457bb76058e8fb52f965408718c1b0461a0382
Eric Roberts <eric@quorum.us>
2023-01-23 17:35:06 -0500

    [notes] 2023-01-23 17:35:06

--- bin/helpers.zsh
+++
@@ -1,9 +0,0 @@
-get_dev_db_password() {
-    grep 'DEV_DB_AND_RDS_TEST_DB_PASSWORD_QUORUM_DJANGO_USER \=' quorum/settings/production_secrets.py | \
-        sed -E 's/[A-Z_]+ \= r?"([a-zA-Z0-9]+)"$/\1/'
-}
-
-die() {
-    print -- "$1" >&2
-    exit 1
-}
--- qrunner/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-DOCKER_HOST_IP="$(dig +short host.docker.internal)"
-if [[ -z $DOCKER_HOST_IP ]]; then
-    DOCKER_HOST_IP="$(ip route | awk '/default/ { print $3 }')"
-    grep -q host.docker.internal /etc/hosts || echo -e "$DOCKER_HOST_IP\thost.docker.internal" >>/etc/hosts
-fi
-[[ -n "$DOCKER_HOST_IP" ]] && echo -e "$(dig +short host.docker.internal)\tpostgres" >>/etc/hosts
-
-exec "$@"
